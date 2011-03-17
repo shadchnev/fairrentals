3087860802019f9384fb08620ff963c33ae7c802
Andrew Nesbitt <andrewnez@gmail.com>
2011-03-17 23:49:31 +0000

    Fixed javascript for new form html

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   $('form').submit(function() {
     payload = {
-      postcode: $('#postcode input').val(),
-      beds: $('#size select').val(),
-      price: $('#price input').val()
+      postcode: $('#postcode').val(),
+      beds: $('#size').val(),
+      price: $('#price').val()
     }
     $('#fair-result').load('/similar-properties', payload);
     return false;
-  })  
+  })
 })
