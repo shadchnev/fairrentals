b6a0574f8862487eec30f41dc9a141687fed6a72
Andrew Nesbitt <andrewnez@gmail.com>
2011-03-18 00:06:24 +0000

    Ensure a nice cache key

--- app.rb
+++ app.rb
@@ -27,7 +27,7 @@ post '/similar-properties' do
   price = params[:price].to_i rescue 0
   return 'meh' unless price > 0
   Search.create(:postcode => params[:postcode], :beds => params[:beds], :price => params[:price])
-  cache_key = "#{params[:postcode].gsub!(/[^a-z0-9\-_]+/, '')}-#{params[:beds]}-prices"
+  cache_key = "#{params[:postcode].gsub!(/[^a-z0-9\-_]+/, '')}-#{params[:beds].to_i}-prices"
   if prices = CACHE.get(cache_key)
   else
     rentals = Zoopla.new('ghj3ebxfkb6q6ndudww6crrc').rentals
